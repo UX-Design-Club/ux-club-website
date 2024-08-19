328a1c16ab3f0671609c5e6505ca9f3e3073814f
DistantLandsProductions <113642297+DistantLandsProductions@users.noreply.github.com>
2024-08-19 12:16:36 -0500

    Removed Placeholder Sponsors

--- app/ui/sponsorreel.tsx
+++ app/ui/sponsorreel.tsx
@@ -11,11 +11,11 @@ export function SponsorReel() {
       <div className="mx-auto py-32 flex flex-col">
         <h1 className="uppercase font-bold text-2xl px-8 md:text-4xl text-center mb-12">Thank you to our sponsors</h1>
         <div className="flex flex-row flex-nowrap gap-4 md:gap-32 justify-center text-slate-600 text-5xl items-center">
-          <FaFigma className="text-teal-600" />
+          {/* <FaFigma className="text-teal-600" />
           <SiAdobe className="text-teal-600" />
           <TbBrandPepsi className="text-teal-600" />
           <SiAdobe  className="text-teal-600"/>
-          <TbBrandPepsi className="text-teal-600" />
+          <TbBrandPepsi className="text-teal-600" /> */}
         </div>
       </div>
     </div>
