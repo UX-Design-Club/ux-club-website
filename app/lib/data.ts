7fa1ebfa467018e44ee26ae01c8236d5c9b679a4
Caitxys <caitlnhartman.ch@gmail.com>
2026-02-11 10:01:42 -0600

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,7 +34,7 @@ export const eventDatabase = [
   new Event(
     "first-general-meeting",
     "Valentine Fundraiser",
-    "Join us for our Valetine Fundraiser! Enjoy some sweet treats and matcha made by our officers!",
+    "Join us for our Valetine Fundraiser! Enjoy some sweet treats and matcha made by our officers! From 11am - 2pm.",
     new Date("2026-02-11"),
     new Date("2024-02-11T11:00:00Z"),
     "",
